7bb15f6248c7d33b3824db48ab580d42a72ed6cd
Emanuel <emanuel.palestino.h@gmail.com>
2023-03-13 23:00:15 -0600

    List card class modified

--- classes/list_card.py
+++ classes/list_card.py
@@ -1,45 +1,9 @@
 import pygame
+from classes.button import Button
 
 
-class ListCard():
+class ListCard(Button):
     def __init__(self, x:int, y:int) -> None:
         # Load background card list image
-        self.image: pygame.Surface = pygame.image.load('images/cards/card_background_yellow.png').convert_alpha()
-        #self.image = pygame.transform.scale(self.image, (248, 136))
-
-        self.rect: pygame.Rect = self.image.get_rect()
-        self.rect.center = (x, y)
-
-        # Flags
-        self.clicked: bool = False
-        self.hovered: bool = False
-
-    def draw(self, surface: pygame.Surface) -> bool:
-        action: bool = False
-
-        # Get mouse position
-        pos: tuple[int][int] = pygame.mouse.get_pos()
-
-        # Check mouseover
-        if self.rect.collidepoint(pos):
-            self.hovered = True
-            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-
-            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-                self.clicked = True
-                action = True
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-
-        else:
-            if self.hovered:
-                self.hovered = False
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-
-        # Check if mouse is no clicked
-        if pygame.mouse.get_pressed()[0] == 0:
-            self.clicked = False
-
-        # Draw card
-        surface.blit(self.image, (self.rect.x, self.rect.y))
-
-        return action
+        background_image = pygame.image.load('images/cards/card_background_yellow.png').convert_alpha()
+        super().__init__(x, y, background_image)
