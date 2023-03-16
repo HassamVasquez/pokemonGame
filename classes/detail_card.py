9ec39a10d85e855c24a0c225fb7ab191743763a4
Emanuel <emanuel.palestino.h@gmail.com>
2023-03-16 08:33:30 -0600

    Showing basic information in detail card

--- classes/detail_card.py
+++ classes/detail_card.py
@@ -23,11 +23,13 @@ class DetailCard():
         self.close_button = ActionButton(512, 660, button_background, 'CLOSE', hover_scale=1.08)
         self.fight_button = ActionButton(768, 660, button_background, 'FIGHT', hover_scale=1.08)
 
+        # Info
+        self.text_font = pygame.font.SysFont('consolas', 24, bold=True)
+
 
     
     
     def draw(self, screen: pygame.Surface, pokemon: Pokemon, showing_details: list[bool]):
-        print(pokemon.name)
         # Translucent background
         screen.blit(self.details_background, (0, 0))
 
@@ -35,6 +37,26 @@ class DetailCard():
         dcb_rect = self.details_card_background.get_rect()
         dcb_rect.center = (640, 360)
         screen.blit(self.details_card_background, dcb_rect.topleft)
+
+        # Pokemon image
+        pokemon_image = pygame.image.load(f'pokemon_jpg/{pokemon.image_path}').convert_alpha()
+        pokemon_image = pygame.transform.scale(pokemon_image, (250, 250))
+        pokemon_image_rect = pokemon_image.get_rect()
+        pokemon_image_rect.center = (640, 160)
+        screen.blit(pokemon_image, pokemon_image_rect.topleft)
+
+        # Info
+        name_text = self.text_font.render(pokemon.name, True, COLOURS.BLACK)
+        screen.blit(name_text, (550, 300))
+
+        type_1_text = self.text_font.render(pokemon.type_1, True, COLOURS.BLACK)
+        screen.blit(type_1_text, (550, 350))
+
+        type_2_text = self.text_font.render(pokemon.type_2, True, COLOURS.BLACK)
+        screen.blit(type_2_text, (550, 400))
+
+        generation_text = self.text_font.render(str(pokemon.generation), True, COLOURS.BLACK)
+        screen.blit(generation_text, (550, 450))
 
         # Close button
         if self.close_button.draw(screen):
