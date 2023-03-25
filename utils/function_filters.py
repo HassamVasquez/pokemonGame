1eded9d9ce1ecbc4f5dd437a1b45ebd1356dc1c4
Octavio Martinez <maho000826@gs.utm.mx>
2023-03-25 13:45:29 -0600

    Filtros actualizados

--- utils/function_filters.py
+++ utils/function_filters.py
@@ -1,8 +1,10 @@
+from classes.pokemon import Pokemon
+
 def filter_type(list_type):
-    def func_pokemon(pokemon):
+    def func_pokemon(pokemon: list[Pokemon]):
         match len(list_type):
             case 1:
-                if(pokemon.type_1 in list_type): return True
+                if(pokemon.type_1 in list_type or pokemon.type_2 in list_type): return True
                 else : return False
             case 2:
                 if(pokemon.type_1 in list_type and pokemon.type_2 in list_type): return True
@@ -13,13 +15,13 @@ def filter_type(list_type):
 
 def filter_gen(list_gen):
     def func_pokemon(pokemon):
-        if(pokemon.generation in list_gen or not list_gen): return True
+        if(str(pokemon.generation) in list_gen or not list_gen): return True
         else : return False
     return func_pokemon
     
 def list_filtred(list_pokemons, dict_filters):
     if dict_filters['type'] or dict_filters['gen']:
-        f_type, f_gen = filter_type(dict_filters['type']), filter_type(dict_filters['gen'])
+        f_type, f_gen = filter_type(dict_filters['type']), filter_gen(dict_filters['gen'])
         filtred = list(map(lambda x:x, filter(lambda y:(f_type(y) and f_gen(y)), list_pokemons)))
         return filtred
     else:
