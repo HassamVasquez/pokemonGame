0246348d774dcd723b5c4cb536c0f1a1fe1ed091
Emanuel <emanuel.palestino.h@gmail.com>
2023-03-27 16:49:57 -0600

    detail_card modified

--- classes/detail_card.py
+++ classes/detail_card.py
@@ -15,16 +15,17 @@ class DetailCard():
         self.details_background.fill(COLOURS.BLACK)
 
         # Details Card
-        self.details_card_background = pygame.Surface((500, 680))
-        self.details_card_background.fill(COLOURS.WHITE)
+        self.details_card_background = pygame.Surface((0, 0))
 
         # Buttons
         button_background = pygame.image.load('images/buttons/button_bg_green.png').convert_alpha()
-        self.close_button = ActionButton(512, 660, button_background, 'CLOSE', hover_scale=1.08)
-        self.select_button = ActionButton(768, 660, button_background, 'SELECT', hover_scale=1.08)
+        self.close_button = ActionButton(512, 640, button_background, 'CLOSE', 0.9, 1.08)
+        self.select_button = ActionButton(768, 640, button_background, 'SELECT', 0.9, 1.08)
 
         # Info
-        self.text_font = pygame.font.SysFont('consolas', 24, bold=True)
+        # Info
+        self.name_font = pygame.font.SysFont('consolas', 28, bold=True)
+        self.text_font = pygame.font.SysFont('consolas', 26, bold=True)
     
     
     def draw(self, screen: pygame.Surface, pokemon: Pokemon, selected_pokemon_list: list[Pokemon], showing_details: list[bool]):
@@ -32,6 +33,7 @@ class DetailCard():
         screen.blit(self.details_background, (0, 0))
 
         # Background details card
+        self.details_card_background = pygame.image.load(f'images/cards/{pokemon.type_1}.png')
         dcb_rect = self.details_card_background.get_rect()
         dcb_rect.center = (640, 360)
         screen.blit(self.details_card_background, dcb_rect.topleft)
@@ -40,21 +42,22 @@ class DetailCard():
         pokemon_image = pygame.image.load(f'pokemon_images/{pokemon.image_path}').convert_alpha()
         pokemon_image = pygame.transform.scale(pokemon_image, (250, 250))
         pokemon_image_rect = pokemon_image.get_rect()
-        pokemon_image_rect.center = (640, 160)
+        pokemon_image_rect.center = (640, 235)
         screen.blit(pokemon_image, pokemon_image_rect.topleft)
 
         # Info
-        name_text = self.text_font.render(pokemon.name, True, COLOURS.BLACK)
-        screen.blit(name_text, (550, 300))
+        name_text = self.name_font.render(pokemon.name, True, COLOURS.BLACK)
+        screen.blit(name_text, (422, 50))
+
+        attack_text = self.text_font.render(str(pokemon.attack), True, COLOURS.BLACK)
+        attack_text_rect = attack_text.get_rect()
+        attack_text_rect.midright  = (814, 62)
+        screen.blit(attack_text, attack_text_rect.topleft)
 
         type_1_text = self.text_font.render(pokemon.type_1, True, COLOURS.BLACK)
-        screen.blit(type_1_text, (550, 350))
-
-        type_2_text = self.text_font.render(pokemon.type_2, True, COLOURS.BLACK)
-        screen.blit(type_2_text, (550, 400))
-
-        generation_text = self.text_font.render(str(pokemon.generation), True, COLOURS.BLACK)
-        screen.blit(generation_text, (550, 450))
+        type_1_text_rect = type_1_text.get_rect()
+        type_1_text_rect.center = (720, 512)
+        screen.blit(type_1_text, type_1_text_rect.topleft)
 
         # Close button
         if self.close_button.draw(screen):
